323beb1eb948d728f71b78e9e50a35426d97422f
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2023-04-12 14:09:13 -0500

    Deployed 4e1bdf3 with MkDocs version: 1.4.2

---
+++ js/extra.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  // Open external links in a new tab
+  $('a[href^="http://"], a[href^="https://"]').not('a[class*=internal]').attr('target', '_blank');
+
+  // https://clipboardjs.com/
+  var selectors = document.querySelectorAll('pre code');
+  var copyButton = '<div class="clipboard"><span class="btn btn-neutral btn-clipboard" title="Copy to clipboard">Copy</span></div>';
+  Array.prototype.forEach.call(selectors, function(selector){
+    selector.insertAdjacentHTML('beforebegin', copyButton);
+  });
+  var clipboard = new ClipboardJS('.btn-clipboard', {
+    target: function (trigger) {
+      return trigger.parentNode.nextElementSibling;
+    }
+  });
+
+  clipboard.on('success', function (e) {
+    e.clearSelection();
+
+    // https://atomiks.github.io/tippyjs/v6/all-props/
+    var tippyInstance = tippy(
+      e.trigger,
+      {
+        content: 'Copied',
+        showOnCreate: true,
+        trigger: 'manual',
+      },
+    );
+    setTimeout(function() { tippyInstance.hide(); }, 1000);
+  });
+});
